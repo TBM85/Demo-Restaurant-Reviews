4ae1c029ba51e9a69627bbd15b5f7a23fbff6d19
TBM85 <tballesterm@gmail.com>
2019-01-08 08:07:16 -0500

    Include some attributes on the restaurants page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -24,32 +24,19 @@ initMap = () => {
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
         mapboxToken: 'pk.eyJ1IjoidGJtODUiLCJhIjoiY2pwbGExYWNxMGJ4dDQybXJlaG9senEwZyJ9.m8bceApLivA3Nyg-YSuSPw',
         maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/" tabindex="-1">OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/" tabindex="-1">CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.mapbox.com/" tabindex="-1">Mapbox</a>',
         id: 'mapbox.streets'    
       }).addTo(newMap);
+
       fillBreadcrumb();
+      addTabindexToMap();
+
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
     }
   });
 }  
- 
-/* window.initMap = () => {
-  fetchRestaurantFromURL((error, restaurant) => {
-    if (error) { // Got an error!
-      console.error(error);
-    } else {
-      self.map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
-        center: restaurant.latlng,
-        scrollwheel: false
-      });
-      fillBreadcrumb();
-      DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
-    }
-  });
-} */
 
 /**
  * Get current restaurant from page URL.
@@ -82,16 +69,23 @@ fetchRestaurantFromURL = (callback) => {
 fillRestaurantHTML = (restaurant = self.restaurant) => {
   const name = document.getElementById('restaurant-name');
   name.innerHTML = restaurant.name;
+  name.setAttribute("tabIndex", "0"); 
+  name.setAttribute("aria-label", "Restaurant name: " + restaurant.name); 
 
   const address = document.getElementById('restaurant-address');
   address.innerHTML = restaurant.address;
+  address.setAttribute("tabIndex", "0"); 
+  address.setAttribute("aria-label", "Restaurant address: " + restaurant.address); 
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = DBHelper.imageAltForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
+  cuisine.setAttribute("tabIndex", "0"); 
+  cuisine.setAttribute("aria-label", "Restaurant cuisine type: " + restaurant.cuisine_type); 
 
   // fill operating hours
   if (restaurant.operating_hours) {
@@ -106,8 +100,12 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
  */
 fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => {
   const hours = document.getElementById('restaurant-hours');
+  hours.setAttribute("tabIndex", "0"); 
+  hours.setAttribute('role', 'listbox');
+  hours.setAttribute('aria-label', 'Schedules');
   for (let key in operatingHours) {
     const row = document.createElement('tr');
+    row.setAttribute("tabIndex", "0"); 
 
     const day = document.createElement('td');
     day.innerHTML = key;
@@ -126,6 +124,9 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
+  container.setAttribute("tabIndex", "0"); 
+  container.setAttribute('role', 'listbox');
+  container.setAttribute('aria-label', 'Reviews');
   const title = document.createElement('h2');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
@@ -149,6 +150,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
+  li.setAttribute("tabIndex", "0"); 
   name.innerHTML = review.name;
   li.appendChild(name);
 
@@ -191,4 +193,21 @@ getParameterByName = (name, url) => {
   if (!results[2])
     return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+/**
+ * Add tabindex attribute to elements in the map
+ */
+addTabindexToMap = () => {
+  const zoomTabindexIn = document.querySelector('.leaflet-control-zoom-in');
+  zoomTabindexIn.setAttribute('tabindex', '-1');
+
+  const zoomTabindexOut = document.querySelector('.leaflet-control-zoom-out');
+  zoomTabindexOut.setAttribute('tabindex', '-1');
+
+  const attributionTabindex = document.querySelector('.leaflet-control-attribution a');
+  attributionTabindex.setAttribute('tabindex', '-1');
+
+  const mapTabindex = document.querySelector('#map');
+  mapTabindex.setAttribute('tabindex', '-1');
 }
